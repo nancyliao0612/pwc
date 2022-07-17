cecdc46e3fe865093f70f85a3b2876720baaa3d5
Nancy <nancy62924@gmail.com>
2022-07-17 14:14:03 +0800

    Add survey form

--- models/Respondents.js
+++ models/Respondents.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const RespondentSchema = new mongoose.Schema({
   name: String,
+  company: String,
+  taxID: String,
+  email: String,
+  address: String,
+  phone: String,
 });
 
 export default mongoose.model("Respondents", RespondentSchema);
